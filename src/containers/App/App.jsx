783f47a61f8acf1473aea3218995de539463b564
Evgeniy Popov <99990148+JENYAODERZHIMIY@users.noreply.github.com>
2023-06-07 13:13:34 +0300

    base route, style

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,31 @@
-import CharacterPage from '../CharacterPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import routesConfig from '../../routes/routesConfig';
+import Header from '../../components/Header';
 
 import styles from './App.module.css';
 
 const App = () => {
   return (
-    <CharacterPage />
+    <>
+      <BrowserRouter>
+        <div className={styles.wrapper}>
+          <Header />
+          <Routes>
+            {routesConfig.map((route, index)=>{
+              return (
+                <Route 
+                  key={index}
+                  path={route.path} 
+                  exact={route.exact}
+                  element={route.component} />
+                )
+            })}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+    
   )
 }
 
